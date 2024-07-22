c90472324641dfe53cc6ea57471391f1366394d0
Copter <copterpanyphon@gmail.com>
2024-07-22 21:29:27 +0700

    create html, css and javascript
    create design of CSS and HTML and add function into this website by using JavaScript

---
+++ new.js
@@ -0,0 +1,9 @@
+let mybutton = document.getElementById('firstbutton')
+let mySecondBotton = document.getElementById('secondbutton');
+
+mybutton.addEventListener('click', function() {
+    window.alert('what is this man')
+});
+mySecondBotton.addEventListener('click', () => {
+    window.alert('My name is Copter')
+});
